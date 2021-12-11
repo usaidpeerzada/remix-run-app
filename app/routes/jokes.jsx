2ca8fe8901854cdd8450ebbaebeaedae5341c7e7
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2021-12-11 11:51:16 +0530

    added auth

--- app/routes/jokes.jsx
+++ app/routes/jokes.jsx
@@ -1,5 +1,11 @@
-import { Link, Outlet, useLoaderData } from "remix";
+import { User } from "@prisma/client";
+import {
+    Link,
+    useLoaderData
+} from "remix";
+import { Outlet } from "remix";
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
 import stylesUrl from "../styles/jokes.css";
 
 export const links = () => {
@@ -11,9 +17,17 @@ export const links = () => {
     ];
 };
 
-export const loader = async () => {
+export const loader = async ({ request }) => {
+    const jokeListItems = await db.joke.findMany({
+        take: 5,
+        orderBy: { createdAt: "desc" },
+        select: { id: true, name: true }
+    });
+    const user = await getUser(request);
+
     const data = {
-        jokeListItems: await db.joke.findMany()
+        jokeListItems,
+        user
     };
     return data;
 };
@@ -35,6 +49,18 @@ export default function JokesRoute() {
                             <span className="logo-medium">J🤪KES</span>
                         </Link>
                     </h1>
+                    {data.user ? (
+                        <div className="user-info">
+                            <span>{`Hi ${data.user?.username}`}</span>
+                            <form action="/logout" method="post">
+                                <button type="submit" className="button">
+                                    Logout
+                                </button>
+                            </form>
+                        </div>
+                    ) : (
+                        <Link to="/login">Login</Link>
+                    )}
                 </div>
             </header>
             <main className="jokes-main">
